30649d0a48cdc43ecfaf34bcbfabf34f8b2fd153
Catalina Arroyo <151681207+caadf@users.noreply.github.com>
2024-03-15 19:22:35 +0000

    empezando el proyecto

--- src/front/js/layout.js
+++ src/front/js/layout.js
@@ -3,6 +3,8 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import ScrollToTop from "./component/scrollToTop";
 import { BackendURL } from "./component/backendURL";
 
+import { Login } from "./pages/login";
+import Register from "./pages/register";
 import { Home } from "./pages/home";
 import { Demo } from "./pages/demo";
 import { Single } from "./pages/single";
@@ -26,6 +28,8 @@ const Layout = () => {
                     <Navbar />
                     <Routes>
                         <Route element={<Home />} path="/" />
+                        <Route element={<Login />} path="/login" />
+                        <Route element={<Register />} path="/register" />
                         <Route element={<Demo />} path="/demo" />
                         <Route element={<Single />} path="/single/:theid" />
                         <Route element={<h1>Not found!</h1>} />
--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -13,7 +13,15 @@ const getState = ({ getStore, getActions, setStore }) => {
 					background: "white",
 					initial: "white"
 				}
-			]
+			],
+			user: null,
+			access_token: null,
+			apiURL: 'https://laughing-spork-g445g5vpvq7q294x6-3001.app.github.dev',
+			name: '',
+			email: '',
+			password: '',
+			password_confirm: '',
+			is_active: true,
 		},
 		actions: {
 			// Use getActions to call a function within a fuction
@@ -46,7 +54,142 @@ const getState = ({ getStore, getActions, setStore }) => {
 
 				//reset the global store
 				setStore({ demo: demo });
-			}
+			},
+
+			/* ACA VA MI CODIGO NUEVO */
+
+			handleSubmitRegister: e => {
+				e.preventDefault()
+				const { name, email, password, is_active, password_confirm, apiURL } = getStore()
+				if (password == password_confirm) {
+					const { getFetch } = getActions()
+					const url = `${apiURL}/api/register`
+					const raw = JSON.stringify({
+						name, email, password, is_active
+					})
+					const options = {
+						method: 'POST',
+						body: raw,
+						headers: {
+							'Content-Type': 'application/json'
+						}
+					}
+
+					const request = getFetch(url, options)
+					request.then((response) => response.json()).then((datos) => {
+						console.log(datos)
+						if (datos.msg) {
+							toast.error(datos.msg)
+						} else {
+
+							toast.success(datos.success)
+							setStore({
+								name: '',
+								email: '',
+								password: '',
+								password_confirm: '',
+								active: true,
+							})
+						}
+
+					}).catch(error => console.log(error))
+
+				}
+				else {
+					toast.error('passwords must be the same')
+				}
+
+			},
+
+			handleChange: e => {
+				const { name, value } = e.target
+				setStore({
+					[name]: value
+				})
+			},
+			handleSubmitLogin: e => {
+				e.preventDefault()
+				const { email, password, apiURL } = getStore()
+				const { getFetch } = getActions()
+
+				const url = `${apiURL}/api/login`
+				const raw = JSON.stringify({
+					email, password
+				})
+
+				const options = {
+					method: 'POST',
+					body: raw,
+					headers: {
+						'Content-Type': 'application/json'
+					}
+				}
+				const request = getFetch(url, options)
+				request.then((response) => response.json()).then((datos) => {
+					console.log(datos)
+
+					if (datos.msg) {
+						toast.error(datos.msg)
+					} else {
+						toast.success(datos.success)
+						setStore({
+							email: '',
+							password: '',
+							access_token: datos.access_token,
+							user: datos.user
+						})
+						sessionStorage.setItem('access_token', datos.access_token)
+						sessionStorage.setItem('user', JSON.stringify(datos.user))
+					}
+				}).catch(error => console.log(error))
+
+			},
+
+			getFetch: (url, options) => {
+				return fetch(url, options)
+			},
+
+			privateRoute: () => {
+                const { apiURL, access_token } = getStore()
+                const url = `${apiURL}/api/me`
+                const options = {
+                    method: 'GET',
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'Authorization': `Bearer ${access_token}`
+                    }
+                }
+                fetch(url, options)
+                    .then(response => {
+                        return response.json()
+                    })
+                    .then(datos => {
+                        if (datos.msg) toast.error(datos.msg)
+                        else {
+                            console.log(datos)
+                            setStore({
+                                user: datos
+                            })
+                        }
+                    })
+            },
+			logout: () => {
+				setStore({
+					user: null,
+					access_token: null
+				})
+				sessionStorage.removeItem('user')
+				sessionStorage.removeItem('access_token')
+			},
+
+			checkCurrentUser: () => {
+				if (sessionStorage.getItem('access_token')) {
+					setStore({
+						access_token: sessionStorage.getItem('access_token'),
+						user: JSON.parse(sessionStorage.getItem('user'))
+					})
+				}
+			},
 		}
 	};
 };
