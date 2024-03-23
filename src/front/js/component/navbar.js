1845df47e42d37825fad77c2cc35249be2558fb5
Carlos Eduardo Tabare Camel <147793216+NickolaiGaming@users.noreply.github.com>
2024-03-23 20:22:46 +0000

    proyecto finalizado

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -1,18 +1,48 @@
-import React from "react";
+import React, { useContext, useEffect } from "react";
 import { Link } from "react-router-dom";
+import { Context } from "../store/appContext";
 
 export const Navbar = () => {
+	const { store, actions } = useContext(Context)
 	return (
-		<nav className="navbar navbar-light bg-light">
-			<div className="container">
-				<Link to="/">
-					<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-				</Link>
-				<div className="ml-auto">
-					<Link to="/demo">
-						<button className="btn btn-primary">Check the Context in action</button>
-					</Link>
-				</div>
+		<nav className="navbar navbar-expand-lg text-light bg-info bg-opacity-25">
+			<div className="container-fluid text-light">
+				<Link className="navbar-brand text-info fs-3" to="/">Inicio</Link>
+				{
+					!!store.user ? (
+						<>
+
+							<button className="navbar-toggler text-dark" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+								<span className="navbar-toggler-icon bg-dark"></span>
+							</button>
+							<div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+								<div class="navbar-nav ms-auto">
+									<Link to="/private" className="nav-link text-dark fs-3 btn btn-outline-success mx-1">Privada</Link>
+									<p className="nav-link text-dark fs-5">Perfil</p>
+									<button className="nav-link bg-secondary text-light rounded-3" onClick={actions.logout}>Logout</button>
+
+
+								</div>
+							</div>
+
+
+						</>
+					) : (
+						<>
+
+							<button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+								<span className="navbar-toggler-icon bg-dark"></span>
+							</button>
+							<div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+								<div class="navbar-nav ms-auto">
+									<Link to="/login" className="nav-link text-dark fs-3 btn btn-outline-success mx-1">Login</Link>
+									<Link to="/register" className="nav-link text-dark fs-3 btn btn-outline-success">Register</Link>
+
+								</div>
+							</div>
+						</>
+					)
+				}
 			</div>
 		</nav>
 	);
--- src/front/js/pages/private.js
+++ src/front/js/pages/private.js
@@ -1,12 +1,14 @@
 import React, { useContext, useEffect } from "react";
 import { Context } from "../store/appContext";
-import { useNavigate } from "react-router-dom"
+import { useNavigate , Navigate } from "react-router-dom"
 
 const Private = () => {
     const { store, actions } = useContext(Context)
     useEffect(() => {
-        if(store?.access_token !== null) actions.privateRoute()
+   //     if(store?.access_token !== null) actions.privateRoute()
     }, [])
+
+    if (store.access_token == null) return <Navigate to="/" replace />
     return (
         <h5>Cuenta</h5>
     )
